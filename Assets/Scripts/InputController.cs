4ca1854d82cf4f87b0f5a7f3f83aa82a9b3ee314
Resvith <28909@s.pm.szczecin.pl>
2023-12-08 14:38:16 +0100

    Add sneaking, prototyping pack

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class InputController : MonoBehaviour
 {
     MovementController movementController;
+    private float originalTargetMovingSpeed;
+    private float targetSneakingSpeed;
     void Start()
     {
         movementController = GetComponent<MovementController>();
+        originalTargetMovingSpeed = movementController.targetMovingSpeed;
+        targetSneakingSpeed = originalTargetMovingSpeed / 2;
     }
 
     void FixedUpdate()
     {
+        MovementController();
+        JumpController();
+        SneakingController();
+    }
+
+    private void MovementController()
+    {
         Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         movementController.Move(targetVelocity);
+    }
 
+    private void JumpController()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             movementController.Jump();
+        }
+    }
 
+    private void SneakingController()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementController.targetMovingSpeed = targetSneakingSpeed;
+        }
+
+        else
+        {
+            movementController.targetMovingSpeed = originalTargetMovingSpeed;
         }
     }
 
