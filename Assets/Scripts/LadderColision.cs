781eaf49f5704db7704c5d135641098f2816dd7c
Resvith <28909@s.pm.szczecin.pl>
2023-12-15 13:00:21 +0100

    Add weapon asset, weapon camera

--- Assets/Scripts/LadderColision.cs
+++ Assets/Scripts/LadderColision.cs
@@ -7,24 +7,24 @@ public class LadderColision : MonoBehaviour
     MovementController movementController;
     private void Start()
     {
-        movementController = GetComponent<MovementController>();
+        //movementController = GetComponent<MovementController>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision != null && collision.collider.name == "Player")
+/*        if (collision != null && collision.collider.name == "Player")
         {
             movementController.isOnLadder = true;
             Debug.Log("Player COllision");
-        }
+        }*/
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision != null && collision.collider.name == "Player")
+/*        if (collision != null && collision.collider.name == "Player")
         {
             movementController.isOnLadder = false;
             Debug.Log("Player Exit");
-        }
+        }*/
     }
 }
