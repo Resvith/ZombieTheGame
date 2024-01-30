d7048cb5b1881ba7e474c8bcb1ceda9eef7d419c
Resvith <116066494+Resvith@users.noreply.github.com>
2024-01-30 12:05:24 +0100

    Add falling detector

---
+++ Assets/FallingDetector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            if (player != null)
+            {
+                player.TakeDamage(999);
+            }
+        }
+
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
